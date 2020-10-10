2987716bef3cd4e182af11b794c7834a142f1e36
Danilkinkin <danilkinkin@gmail.com>
2020-10-11 01:25:38 +0400

    Change api address for prod

--- src/config.js
+++ src/config.js
@@ -1,3 +1,1 @@
-export default {
-    server_url: PRODUCTION_MODE ? 'http://rigami.io:8080' : 'http://localhost:8080',
-};
+export default { server_url: PRODUCTION_MODE ? 'https://api.rigami.io' : 'http://localhost:8080' };
