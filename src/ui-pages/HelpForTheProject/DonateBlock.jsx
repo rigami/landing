4ccba16e509fa4e51735d23391b2932bddf1341b
Danilkinkin <danilkinkin@gmail.com>
2021-02-28 23:27:04 +0400

    Change style in donate block

--- src/ui-pages/HelpForTheProject/DonateBlock.jsx
+++ src/ui-pages/HelpForTheProject/DonateBlock.jsx
@@ -2,28 +2,30 @@ import React from 'react';
 import ContentCard from '@/ui-components/ContentCard';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link, List, ListItem } from '@material-ui/core';
+import {
+    Link,
+    Box,
+    GridList,
+    GridListTile,
+    CardMedia,
+} from '@material-ui/core';
 import { ArrowForwardRounded as ArrowIcon } from '@material-ui/icons';
-import donateImageUrl from '@/resources/clear-donate-background.png';
 import { withTranslation } from 'next-i18next';
+import tinkoffImageSrc from '@/resources/tinkoff-icon.png';
+import paypalImageSrc from '@/resources/paypal-icon.png';
+import bootsyImageSrc from '@/resources/bootsy-icon.png';
+import kofiImageSrc from '@/resources/kofi-icon.png';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        background: `url(${donateImageUrl}), linear-gradient(241.37deg, #F7F7F7 27.72%, #EBEBEC 81.74%)`,
-        backgroundSize: '470px, 100%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '100% 100%',
         minHeight: 480,
-        [theme.breakpoints.down('xs')]: {
-            backgroundSize: '300px, 100%',
-            minHeight: 360,
-        },
+        [theme.breakpoints.down('xs')]: { minHeight: 360 },
     },
     subtitle: { maxWidth: 470 },
     actions: { paddingTop: theme.spacing(16) },
-    list: {
+    wrapper: {
         marginTop: theme.spacing(4),
-        maxWidth: 450,
+        maxWidth: 520,
     },
     link: {
         display: 'inline-block',
@@ -32,8 +34,34 @@ const useStyles = makeStyles((theme) => ({
     },
     linkIcon: {
         marginLeft: theme.spacing(1),
-        width: 20,
-        height: 20,
+        width: 28,
+        height: 28,
+    },
+    card: {
+        height: 84,
+        width: 260,
+    },
+    block: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        alignItems: 'end',
+        justifyContent: 'flex-end',
+    },
+    text: {
+        fontSize: '1.1rem',
+        display: 'flex',
+        alignItems: 'center',
+        fontWeight: 700,
+        marginTop: theme.spacing(2),
+    },
+    image: {
+        height: 70,
+        width: 'auto',
+    },
+    gridItem: {
+        background: 'none',
+        padding: `${theme.spacing(2)}px 0`,
     },
 }));
 
@@ -51,33 +79,44 @@ function DonateBlock({ t, className: externalClassname }) {
             subtitle={t('donate.description')}
             disableTextBackdrop
         >
-            <List dense className={classes.list} disablePadding>
-                {[
-                    {
-                        url: 'https://www.tinkoff.ru/sl/1zAH3HyZ54Q',
-                        title: t('donate.list.tinkoff'),
-                    },
-                    {
-                        url: 'paypal.me/danilkinkin',
-                        title: t('donate.list.paypal'),
-                    },
-                    {
-                        url: 'https://boosty.to/danilkinkin',
-                        title: t('donate.list.bootsy'),
-                    },
-                    {
-                        url: 'https://ko-fi.com/danilkinkin',
-                        title: t('donate.list.koFi'),
-                    },
-                ].map(({ url, title }) => (
-                    <ListItem key={url} disableGutters>
-                        <Link href={url} target="_blank" className={classes.link}>
-                            {title}
-                            <ArrowIcon className={classes.linkIcon} />
-                        </Link>
-                    </ListItem>
-                ))}
-            </List>
+            <Box className={classes.wrapper}>
+                <GridList cols={2} spacing={1}>
+                    {[
+                        {
+                            url: 'https://www.tinkoff.ru/sl/1zAH3HyZ54Q',
+                            title: t('donate.list.tinkoff'),
+                            image: tinkoffImageSrc,
+                        },
+                        {
+                            url: 'paypal.me/danilkinkin',
+                            title: t('donate.list.paypal'),
+                            image: paypalImageSrc,
+                        },
+                        {
+                            url: 'https://boosty.to/danilkinkin',
+                            title: t('donate.list.bootsy'),
+                            image: bootsyImageSrc,
+                        },
+                        {
+                            url: 'https://ko-fi.com/danilkinkin',
+                            title: t('donate.list.koFi'),
+                            image: kofiImageSrc,
+                        },
+                    ].map(({ title, url, image }) => (
+                        <GridListTile key={url} classes={{ tile: classes.gridItem }} style={{ height: 'auto' }}>
+                            <Link href={url} target="_blank" color="textPrimary">
+                                <Box className={classes.block}>
+                                    <CardMedia className={classes.image} image={image} component="img" />
+                                    <span className={classes.text}>
+                                        {title}
+                                        <ArrowIcon className={classes.linkIcon} />
+                                    </span>
+                                </Box>
+                            </Link>
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </Box>
         </ContentCard>
     );
 }
